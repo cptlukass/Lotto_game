8b132575e38d2819e787d8fbb9af078111acdca5
cptlukass <70337669+cptlukass@users.noreply.github.com>
2021-02-23 23:33:35 +0100

    Commit

---
+++ Zadanie 2.py
@@ -0,0 +1,40 @@
+from random import randint
+
+
+def lotto():
+    """Collect 6 numbers from the user, then pick 6 random numbers
+    and inform the user how many numbers he guessed.
+
+    :return: user numbers list, randomly picked numbers list, information about user hits.
+    """
+    str_index = 1
+    num_list = []
+    while len(num_list) < 6:
+        try:
+            user_num = input(f"Wytypuj liczbę nr {str_index}: ")
+            input_num = int(user_num)
+            if input_num not in num_list and input_num in range(1, 50):
+                num_list.append(input_num)
+                str_index += 1
+            else:
+                print("Liczba się powtarza lub jest spoza zakresu!")
+        except ValueError:
+            print("Podana wartość nie jest liczbą!")
+    num_list.sort()
+    print(num_list)
+    winning_list = []
+    for i in range(6):
+        bump = randint(1, 49)
+        winning_list.append(bump)
+    winning_list.sort()
+    print(winning_list)
+    strike = 0
+    for i in range(6):
+        if num_list[i] in winning_list:
+            strike += 1
+        else:
+            continue
+    return f"You hit {strike} numbers in this lottery!"
+
+
+print(lotto())
